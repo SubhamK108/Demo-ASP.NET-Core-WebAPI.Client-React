eebb35f435886115841755d3f6f3e407f407b8c3
Subham Karmakar <subhamk.subs@gmail.com>
2021-02-23 23:56:35 +0530

    Added more stuff in Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,11 +7,13 @@ const Home: React.FC = () => {
         <div>
             <h1>This is a Demo App made in:</h1>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">ASP.NET Core with C#</li>
-                <li className="list-group-item">React with TypeScript</li>
-                <li className="list-group-item">PostgreSQL</li>
+                <li className="list-group-item">ASP.NET Core with C# in the Backend</li>
+                <li className="list-group-item">React with TypeScript in the Frontend</li>
+                <li className="list-group-item">Bootstrap for styling</li>
+                <li className="list-group-item">PostgreSQL as the Database</li>
+                <li className="list-group-item">Docker</li>
                 <li className="list-group-item">GitHub Pages</li>
-                <li className="list-group-item">Heroku</li>
+                <li className="list-group-item">Heroku for Hosting</li>
             </ul>
         </div>
     );
