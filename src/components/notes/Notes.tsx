f496ce97ac2b0a829600f45cfb9496ee66912346
Subham Karmakar <subhamk.subs@gmail.com>
2021-06-22 21:46:48 +0530

    Created new Notes component

---
+++ src/components/notes/Notes.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+function Notes(): ReactElement {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>YOUR NOTES</h1>
+        </div>
+    );
+}
+
+export default Notes;
